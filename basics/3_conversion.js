846519d8120afe4f11348565c97548f8a7a5d35e
amitsingh1759 <amitkumar1451@outlook.com>
2026-03-18 06:53:58 +0000

    conversion datatype

--- basics/3_conversion.js
+++ basics/3_conversion.js
@@ -26,3 +26,23 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hellow"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" +2 + 2);
+console.log(1 + 2 + "2");
+
+
+
+
+
+
